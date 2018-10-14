b3fd914315f31d8cdb65a5e5503d0321a6811792
manhtai <vo@manhtai.com>
2018-10-14 22:42:17 +0700

    Allow to turn on and off the blink

--- esp8266_led/esp8266_led.ino
+++ esp8266_led/esp8266_led.ino
@@ -1,29 +1,44 @@
+int ledState = 0;
+int btnPin = 0;
+int myLeds[] = {D1, D2, D5, D7};
+
+void blink() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(myLeds[i], HIGH);
+    delay(1000);
+    digitalWrite(myLeds[i], LOW);
+    delay(1000);  
+  }
+}
+
+void off() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(myLeds[i], LOW);
+  }
+}
+
+void toggle() {
+  ledState = !ledState;
+}
+
 void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(myLeds[i], OUTPUT);
+  }
+
+  // Attach Flash button to input
+  pinMode(btnPin, INPUT_PULLUP);
+  attachInterrupt(btnPin, toggle, FALLING);
+  
   Serial.begin(9600);
-  pinMode(D1, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D7, OUTPUT);  
+  Serial.setDebugOutput(true);
 }
 
 void loop() {
-  digitalWrite(D1, HIGH);
-  delay(1000);
-  digitalWrite(D1, LOW);
-  delay(1000);
-
-  digitalWrite(D3, HIGH);
-  delay(1000);
-  digitalWrite(D3, LOW);
-  delay(1000);
-   
-  digitalWrite(D5, HIGH);
-  delay(1000);
-  digitalWrite(D5, LOW);
-  delay(1000);
-
-  digitalWrite(D7, HIGH);
-  delay(1000);
-  digitalWrite(D7, LOW);
-  delay(1000);
+  Serial.println(ledState);
+  if (ledState) {
+    blink();
+  } else {
+    off();
+  }
 }
